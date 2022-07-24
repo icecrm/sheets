6ac27e8a13d2810f26f4d374f4adb8128654f2bb
Ice Crm <81833740+icecrm@users.noreply.github.com>
2022-07-24 15:24:40 -0500

    Update rusha.lua

--- rusha.lua
+++ rusha.lua
@@ -7,10 +7,7 @@ shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
 shared.scr = [[
- 9 [qe] 6 [qe] 5-6-*-[29] [qe] 6 [qe] - [9p] p [qep] p [6p] p [qep] p [9p] p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] [qed] [9g] [qe] 6 [qej] [*h] [weg] [6f] [weh] [9g] [qef] [6d] [qeg] [0f] [0WG] [7H] [9Wf] [*0ej] - [30e] 0 p p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] P [qep] O [9p] P [6p] O [qep] [qeg] [9j] [qe] 6 [qeg] [wJ] [Eyh] [qj] [eyg] [0h] [qEf] [9g] [qed] [60f] [7qd] [*TS] [60f] [9qyd] - 6-9-0  p p [qep] p [6p] p  [qep] p [9p] p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] [qed] [9g] [qe] 6 [qej] [*h] [weg] [6f] [weh] [9g] [qef] [6d] [qeg] [0f] [0WG]
-[7H] [9Wf] [*0ej] - [60e] 9-0 p p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] P [qep] O [9p] P [qep] O [6p] [qeg] [9j] [qe] 6 [qeg] [wJ]
-[Eyh] [qj] [eyg] [0h] [wEf] [9g] [qed] [60f] [7qd] [*TS] [60f] [9qyd] - [29] [wdJ] [EPyh] [9dJ] [EPyh] [wdJ] [EPyh] [9dJ] [EPyh]
-[wdJ] [EPyh] [9sj] [EDyl] [wdJ] [Ey] [P9h] [Ey] [P9h] [Ey] [qgj] [eyfH] [9gj] [eyhJ] [qgj] [ey] [9dg] [ey] [5wPh] [6epg] [^Eof]
-[5wPh] [6eSj] - [6e] [wdJ] [EPyh] [9dJ] [EPyh] [wdJ] [EPyh] [9dJ] [EPyh] [qgj] [eyfH] [9gj] [eyhJ] [qgj] [ey] [9dg] [ey] [EJyh] [EydJ] 
-[epSj] [woPh] [qigp] [0ufo] [9ydi] [6epj] [9ydgz]
+[30] [wr] 7 [wr] [30] [wr] 7 [wr] [0a] a [wra] a [7a] a [wra] a [7a] [wrP][7a] [wrs] [0a] [wro] [7u] [wro] [0a] [wr] 7 [wro] [Qp]
+[erI] [7Y] [erI] [Qp] [er] 7 [erI] [o] [wrI] [7u] [wro] [(QrI] 7 - [37r] - 6-$-^-7-0 a a [wra] a [7a] a [wra] a [0a][wrP][7a] [wrs] [0a] [wro]
+[7u] [wro] [0a][wr] 7 [wro] 6 [0tp] 8 [0tI] [7a][(tp][7ro][(eI][30wu] [7rua] [0uof]
 ]]
