f3b89b75986f4a67c15b9eee2745e26bbc6d6533
Ice Crm <81833740+icecrm@users.noreply.github.com>
2022-07-24 15:22:47 -0500

    Create rusha.lua

---
+++ rusha.lua
@@ -0,0 +1,16 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+ 9 [qe] 6 [qe] 5-6-*-[29] [qe] 6 [qe] - [9p] p [qep] p [6p] p [qep] p [9p] p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] [qed] [9g] [qe] 6 [qej] [*h] [weg] [6f] [weh] [9g] [qef] [6d] [qeg] [0f] [0WG] [7H] [9Wf] [*0ej] - [30e] 0 p p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] P [qep] O [9p] P [6p] O [qep] [qeg] [9j] [qe] 6 [qeg] [wJ] [Eyh] [qj] [eyg] [0h] [qEf] [9g] [qed] [60f] [7qd] [*TS] [60f] [9qyd] - 6-9-0  p p [qep] p [6p] p  [qep] p [9p] p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] [qed] [9g] [qe] 6 [qej] [*h] [weg] [6f] [weh] [9g] [qef] [6d] [qeg] [0f] [0WG]
+[7H] [9Wf] [*0ej] - [60e] 9-0 p p [qep] p [6p] p [qep] p [9p] P [qep] O [6p] P [qep] O [9p] P [qep] O [6p] [qeg] [9j] [qe] 6 [qeg] [wJ]
+[Eyh] [qj] [eyg] [0h] [wEf] [9g] [qed] [60f] [7qd] [*TS] [60f] [9qyd] - [29] [wdJ] [EPyh] [9dJ] [EPyh] [wdJ] [EPyh] [9dJ] [EPyh]
+[wdJ] [EPyh] [9sj] [EDyl] [wdJ] [Ey] [P9h] [Ey] [P9h] [Ey] [qgj] [eyfH] [9gj] [eyhJ] [qgj] [ey] [9dg] [ey] [5wPh] [6epg] [^Eof]
+[5wPh] [6eSj] - [6e] [wdJ] [EPyh] [9dJ] [EPyh] [wdJ] [EPyh] [9dJ] [EPyh] [qgj] [eyfH] [9gj] [eyhJ] [qgj] [ey] [9dg] [ey] [EJyh] [EydJ] 
+[epSj] [woPh] [qigp] [0ufo] [9ydi] [6epj] [9ydgz]
+]]
